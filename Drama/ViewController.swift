77ba287a7a8ae6648be8f699286609787a34ccd3
도미닉 <kiljh@me.com>
2019-11-17 21:59:57 +0900

    프로젝트 생성

---
+++ Drama/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Drama
+//
+//  Created by joon-ho kil on 2019/11/17.
+//  Copyright © 2019 joon-ho kil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
