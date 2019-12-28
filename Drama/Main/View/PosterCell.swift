36f24aa44f6e066d9c9a712ac6386013142bc35e
도미닉 <kiljh@me.com>
2019-12-29 00:13:56 +0900

    메인 페이지 구현

---
+++ Drama/Main/View/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Drama
+//
+//  Created by joon-ho kil on 2019/12/28.
+//  Copyright © 2019 joon-ho kil. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet var posterImageView: UIImageView!
+    
+}
---
+++ Drama/Main/View/SubscribeCell.swift
@@ -0,0 +1,15 @@
+//
+//  SubscribeCell.swift
+//  Drama
+//
+//  Created by joon-ho kil on 2019/12/28.
+//  Copyright © 2019 joon-ho kil. All rights reserved.
+//
+
+import UIKit
+
+class SubscribeCell: UICollectionViewCell {
+    @IBOutlet var posterImageView: UIImageView!
+    @IBOutlet var title: UILabel!
+    
+}
