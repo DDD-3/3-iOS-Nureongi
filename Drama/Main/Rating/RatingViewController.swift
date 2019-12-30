37cfaea4005154d258bff51ac557dd6d815668e2
KiwookJUNG <ismeeki17@naver.com>
2019-12-30 22:01:48 +0900

    1. Charts Pod 설치
    2. ChartCell 구현
    
    3. 커스터마이징 및 코드 리팩토링이 필요해보임.

--- Drama/Main/Rating/RatingViewController.swift
+++ Drama/Main/Rating/RatingViewController.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 import XLPagerTabStrip
+import SnapKit
 import Charts
 
 class RatingViewController: UIViewController, IndicatorInfoProvider {
     //MARK: - Views Start
   
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.contentSize = self.contentView.bounds.size
+        return scrollView
+    }()
+    
+    lazy var contentView: UIView = {
+        let contentView = UIView()
+        return contentView
+    }()
+    
+    var averageGrade: ChartView = {
+        let average = ChartView(type: .average)
+        return average
+    }()
+    
+    var episodeGrade: ChartView = {
+        let episode = ChartView(type: .episode)
+        return episode
+    }()
+    
+    var myGrade: ChartView = {
+        let my = ChartView(type: .my)
+        return my
+    }()
+        
     //MARK: - Views End
 
     
@@ -21,6 +48,14 @@ class RatingViewController: UIViewController, IndicatorInfoProvider {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.addSubview(scrollView)
+        self.scrollView.addSubview(contentView)
+        
+        self.contentView.addSubview(averageGrade)
+        self.contentView.addSubview(episodeGrade)
+        self.contentView.addSubview(myGrade)
+        
+        setupAutoLayout()
     }
     
     func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
@@ -32,7 +67,31 @@ class RatingViewController: UIViewController, IndicatorInfoProvider {
 
 extension RatingViewController {
     private func setupAutoLayout() {
-     
+        scrollView.snp.makeConstraints { (make) in
+            make.top.leading.trailing.bottom.equalTo(self.view.safeAreaLayoutGuide)
+        }
+        
+        contentView.snp.makeConstraints { (make) in
+            make.top.leading.trailing.bottom.equalToSuperview()
+            make.width.equalTo(self.view.snp.width)
+        }
+        
+        averageGrade.snp.makeConstraints { (make) in
+            make.top.leading.trailing.equalToSuperview()
+            make.height.equalTo(280)
+        }
+        
+        episodeGrade.snp.makeConstraints { (make) in
+            make.top.equalTo(averageGrade.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(280)
+        }
+        
+        myGrade.snp.makeConstraints { (make) in
+            make.top.equalTo(episodeGrade.snp.bottom)
+            make.leading.trailing.bottom.equalToSuperview()
+            make.height.equalTo(280)
+        }
       
     }
     
