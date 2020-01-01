5f220d7c67116f6ca412d756074c180893cf1d78
KiwookJUNG <ismeeki17@naver.com>
2020-01-01 15:10:05 +0900

    TODO : 모델 구현

---
+++ Drama/Main/Post/Model/Post.swift
@@ -0,0 +1,15 @@
+//
+//  Post.swift
+//  Drama
+//
+//  Created by 정기욱 on 2020/01/01.
+//  Copyright © 2020 joon-ho kil. All rights reserved.
+//
+
+import Foundation
+
+struct Post: Codable {
+    
+    // TODO: 모델 구현
+    
+}
